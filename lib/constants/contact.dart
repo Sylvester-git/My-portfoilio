c18450d55d6edac5c60d550a22812d18319a6536
Sylvesterpaul David <sylvesterpauldaviditzspd123@gmail.com>
2022-11-26 14:53:25 +0100

    update

--- lib/constants/contact.dart
+++ lib/constants/contact.dart
@@ -1,25 +1,42 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_portfolio/Widgets/contactmecontainer.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:universal_html/html.dart' as html;
 
 whatsappcontact() async {
   final Uri whatsappnumber = Uri.parse('https://wa.me/+2347036600085');
-
-  await launchUrl(whatsappnumber);
+  if (kIsWeb) {
+    html.window.open('https://wa.me/+2347036600085', '_blank');
+  } else {
+    launchUrl(whatsappnumber);
+  }
 }
 
 linkind() async {
   final Uri linkedin = Uri.parse(
       'https://www.linkedin.com/in/sylvester-paul-ebinehita-1176ab221');
 
-  await launchUrl(linkedin);
+  if (kIsWeb) {
+    html.window.open(
+        'https://www.linkedin.com/in/sylvester-paul-ebinehita-1176ab221',
+        '_blank');
+  } else {
+    launchUrl(linkedin);
+  }
 }
 
 github() async {
   final Uri git_hub = Uri.parse('https://github.com/Itzspd?tab=repositories');
-
-  await launchUrl(git_hub);
+  if (kIsWeb) {
+    html.window.open(
+      'https://github.com/Itzspd?tab=repositories',
+      '_blank',
+    );
+  } else {
+    launchUrl(git_hub);
+  }
 }
 
 Widget contactme() {
